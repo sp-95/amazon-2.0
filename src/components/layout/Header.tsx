793d760f345bce1ebbe6571a206ea85b27cb09b5
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-25 11:15:10 +0545

    Feat: Create a header with amazon logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header: React.FunctionComponent = () => {
-  return <header>Header</header>
+  return (
+    <header>
+      <div className="flex items-center bg-amazon-blue-900 p-2">
+        <div className="mt-2 flex items-center">
+          <Image
+            src="/assets/images/amazon.png"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      <div />
+    </header>
+  )
 }
 
 export default Header
