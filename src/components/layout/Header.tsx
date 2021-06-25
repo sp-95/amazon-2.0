75bfd5036ceca15bc9490d01cfd2db6043306428
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-25 12:20:15 +0545

    Feat: Add search bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import Image from 'next/image'
+import React from 'react'
+import { SearchIcon } from '@heroicons/react/outline'
 
 const Header: React.FunctionComponent = () => {
   return (
     <header>
-      <div className="flex items-center bg-amazon-blue-900 p-2">
+      <div className="p-2 flex items-center bg-amazon-blue-900">
         <div className="mt-2 flex items-center">
           <Image
             src="/assets/images/amazon.png"
@@ -13,6 +14,14 @@ const Header: React.FunctionComponent = () => {
             objectFit="contain"
             className="cursor-pointer"
           />
+        </div>
+
+        <div className="flex h-11 w-full focus-within:ring focus-within:ring-amazon-orange-400 rounded-md">
+          <input
+            type="text"
+            className="p-2.5 rounded-l-md w-full focus:outline-none"
+          />
+          <SearchIcon className="h-full p-2 px-2.5 bg-amazon-orange-300 hover:bg-amazon-orange-400 rounded-r-md" />
         </div>
       </div>
       <div />
