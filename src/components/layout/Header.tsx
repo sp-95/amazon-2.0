e4af4c7d879f3c0cf7d80fa7fb9e488b9309a93a
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-25 13:18:32 +0545

    Feat: Add all the header items

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,27 +1,49 @@
 import Image from 'next/image'
 import React from 'react'
-import { SearchIcon } from '@heroicons/react/outline'
+import { SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
 
 const Header: React.FunctionComponent = () => {
   return (
-    <header>
-      <div className="p-2 flex items-center bg-amazon-blue-900">
-        <div className="mt-2 flex items-center">
+    <header className="bg-amazon-blue-900">
+      <div className="p-2 mr-1 flex items-center space-x-4">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 flex-shrink-0">
           <Image
             src="/assets/images/amazon.png"
-            width={150}
+            width={110}
             height={40}
             objectFit="contain"
             className="cursor-pointer"
           />
         </div>
 
-        <div className="flex h-11 w-full focus-within:ring focus-within:ring-amazon-orange-400 rounded-md">
+        <div className="hidden sm:flex h-11 w-full focus-within:ring focus-within:ring-amazon-orange-400 rounded-md">
           <input
             type="text"
             className="p-2.5 rounded-l-md w-full focus:outline-none"
           />
-          <SearchIcon className="h-full p-2 px-2.5 bg-amazon-orange-300 hover:bg-amazon-orange-400 rounded-r-md" />
+          <SearchIcon className="h-full p-2 px-2.5 cursor-pointer bg-amazon-orange-300 hover:bg-amazon-orange-400 rounded-r-md" />
+        </div>
+
+        <div className="flex items-center space-x-4 text-white">
+          <div className="cursor-pointer">
+            <div>
+              <p className="m-0 text-xs whitespace-nowrap">Hello, Sign in</p>
+            </div>{' '}
+            <p className="m-0 text-sm font-bold whitespace-nowrap">
+              Accounts &#38; Lists
+            </p>
+          </div>
+          <div className="cursor-pointer">
+            <div>
+              <p className="m-0 text-xs whitespace-nowrap">Returns</p>
+            </div>
+            <p className="m-0 text-sm font-bold whitespace-nowrap">
+              &#38; Orders
+            </p>
+          </div>
+          <div className="cursor-pointer">
+            <ShoppingCartIcon className="h-full w-10" />
+          </div>
         </div>
       </div>
       <div />
