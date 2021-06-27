970d3b0a583a8eb1ba050e3da00bc71e4d779a15
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-28 01:02:41 +0545

    Style: Update margins and sizes

--- src/components/home/ProductFeed.tsx
+++ src/components/home/ProductFeed.tsx
@@ -12,7 +12,7 @@ export default function ProductFeed(
   const { products } = props
 
   return (
-    <section className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52">
+    <section className="product-container">
       {products.map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,6 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from '@heroicons/react/outline'
-import Image from 'next/image'
 import React from 'react'
 
 const Header: React.FunctionComponent = () => {
@@ -11,7 +10,11 @@ const Header: React.FunctionComponent = () => {
     <header>
       <nav className="nav__top">
         <div className="nav__logo">
-          <Image src="/assets/images/amazon.png" width={110} height={40} />
+          <img
+            src="/assets/images/amazon.png"
+            alt="Amazon Logo"
+            className="h-9"
+          />
         </div>
 
         <div className="nav__search__container">
@@ -21,36 +24,38 @@ const Header: React.FunctionComponent = () => {
           </span>
         </div>
 
-        <div className="nav__button--top">
-          <div>
-            <p className="nav__text--small">Hello, Sign in</p>
-          </div>{' '}
-          <p className="nav__text--bold">Accounts &amp; Lists</p>
-        </div>
-
-        <div className="nav__button--top nav__hidden">
-          <div>
-            <p className="nav__text--small">Returns</p>
+        <div className="flex items-center">
+          <div className=" nav__button">
+            <div>
+              <p className="nav__text--small">Hello, Sign in</p>
+            </div>{' '}
+            <p className="nav__text--bold">Accounts &amp; Lists</p>
           </div>
-          <p className="nav__text--bold">&amp; Orders</p>
-        </div>
 
-        <div className="nav__item-counter__container">
-          <span className="nav__item-counter">0</span>
-          <ShoppingCartIcon className="h-full" />
+          <div className=" nav__button nav__hidden">
+            <div>
+              <p className="nav__text--small">Returns</p>
+            </div>
+            <p className="nav__text--bold">&amp; Orders</p>
+          </div>
+
+          <div className="relative nav__button">
+            <span className="nav__item-counter">0</span>
+            <ShoppingCartIcon className="h-9" />
+          </div>
         </div>
       </nav>
 
       <nav className="nav__bottom">
-        <span className="nav__button--bottom flex items-center">
-          <MenuIcon className="h-full mr-0.5" />
+        <span className="nav__button--bottom flex items-center py-0.5">
+          <MenuIcon className="h-6 mr-0.5" />
           <p className="nav__text">All</p>
         </span>
         <p className="nav__text nav__button--bottom">Today&apos;s Deals</p>
-        <p className="nav__text nav__button--bottom">Gift Cards</p>
         <p className="nav__text nav__button--bottom nav__hidden">
           Customer Service
         </p>
+        <p className="nav__text nav__button--bottom">Gift Cards</p>
         <p className="nav__text nav__button--bottom nav__hidden">Registry</p>
         <p className="nav__text nav__button--bottom nav__hidden">Sell</p>
       </nav>
