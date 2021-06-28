7afc727091565e8525d54354f7240c78da038a27
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-28 13:52:22 +0545

    Feat: Add multi-column spanning product cards and promo banner

--- src/components/home/ProductFeed.tsx
+++ src/components/home/ProductFeed.tsx
@@ -12,17 +12,53 @@ export default function ProductFeed(
   const { products } = props
 
   return (
-    <section className="product__section">
-      {products.map(({ id, title, price, description, category, image }) => (
-        <Product
-          key={id}
-          title={title}
-          price={price}
-          description={description}
-          category={category}
-          image={image}
-        />
-      ))}
+    <section className="product__grid">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+
+      <img
+        src="/assets/images/banners/promo_banner.jpg"
+        alt="AD"
+        className="col-span-full"
+      />
+
+      <div className="sm:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </section>
   )
 }
