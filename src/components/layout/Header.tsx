cff8cb5945eda4bb0f8255191df6789773f9a8b7
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-28 12:40:40 +0545

    Style: Change header styles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,58 +6,80 @@ import {
 import React from 'react'
 
 const Header: React.FunctionComponent = () => {
+  const [searchFocused, setSearchFocused] = React.useState(false)
+
   return (
     <header>
       <nav className="nav__top">
-        <div className="nav__logo">
+        <a href="/" className="nav__logo">
           <img
             src="/assets/images/amazon.png"
             alt="Amazon Logo"
             className="h-9"
           />
-        </div>
+        </a>
 
-        <div className="nav__search__container">
+        <form
+          className={`nav__search__container ${
+            searchFocused && 'focus-within:ring-0'
+          }`}
+        >
           <input type="text" className="nav__search__input" />
-          <span className="nav__search__button">
+          <button
+            type="submit"
+            className="nav__search__button"
+            onFocus={() => setSearchFocused(true)}
+            onBlur={() => setSearchFocused(false)}
+          >
             <SearchIcon className="h-7 px-2.5" />
-          </span>
-        </div>
+          </button>
+        </form>
 
         <div className="flex items-center">
-          <div className=" nav__button">
+          <a href="/" className="nav__button">
             <div>
               <p className="nav__text--small">Hello, Sign in</p>
             </div>{' '}
             <p className="nav__text--bold">Accounts &amp; Lists</p>
-          </div>
+          </a>
 
-          <div className=" nav__button nav__hidden">
+          <a href="/" className="nav__button nav__hidden">
             <div>
               <p className="nav__text--small">Returns</p>
             </div>
             <p className="nav__text--bold">&amp; Orders</p>
-          </div>
+          </a>
 
-          <div className="relative nav__button">
+          <a href="/" className="relative nav__button">
             <span className="nav__item-counter">0</span>
             <ShoppingCartIcon className="h-9" />
-          </div>
+          </a>
         </div>
       </nav>
 
       <nav className="nav__bottom">
-        <span className="nav__button--bottom flex items-center py-0.5">
+        <a
+          href="/"
+          className="nav__button--bottom flex items-center py-0.5 focus:text-white"
+        >
           <MenuIcon className="h-6 mr-0.5" />
           <p className="nav__text">All</p>
-        </span>
-        <p className="nav__text nav__button--bottom">Today&apos;s Deals</p>
-        <p className="nav__text nav__button--bottom nav__hidden">
+        </a>
+        <a href="/" className="nav__text nav__button--bottom">
+          Today&apos;s Deals
+        </a>
+        <a href="/" className="nav__text nav__button--bottom nav__hidden">
           Customer Service
-        </p>
-        <p className="nav__text nav__button--bottom">Gift Cards</p>
-        <p className="nav__text nav__button--bottom nav__hidden">Registry</p>
-        <p className="nav__text nav__button--bottom nav__hidden">Sell</p>
+        </a>
+        <a href="/" className="nav__text nav__button--bottom">
+          Gift Cards
+        </a>
+        <a href="/" className="nav__text nav__button--bottom nav__hidden">
+          Registry
+        </a>
+        <a href="/" className="nav__text nav__button--bottom nav__hidden">
+          Sell
+        </a>
       </nav>
     </header>
   )
