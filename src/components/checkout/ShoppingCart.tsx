bbbf7d57f30e30e9a01bc5990ec5776beefc4e6c
Shashanka Prajapati <shashankap95@gmail.com>
2021-07-01 11:54:28 +0545

    Feat: Ability to select or unselect every item in the cart

--- src/components/checkout/ShoppingCart.tsx
+++ src/components/checkout/ShoppingCart.tsx
@@ -1,17 +1,27 @@
 import CheckoutProduct from '@/components/product/CheckoutProduct'
-import { selectItems } from '@/slices/cartSlice'
+import { selectItems, updateInCart } from '@/slices/cartSlice'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Subtotal from './Subtotal'
 
 function ShoppingCart(): React.ReactElement {
   const items = useSelector(selectItems)
+  const dispatch = useDispatch()
+
+  const handleSelectAll = () => {
+    const allSelected = items.every((item) => item.checked)
+    items.forEach((item) =>
+      dispatch(updateInCart({ ...item, checked: !allSelected }))
+    )
+  }
 
   return (
     <div className="p-5">
       <h1>Shopping Cart</h1>
-      <button type="button" className="amazon-link">
-        Deselect all items
+      <button type="button" className="amazon-link" onClick={handleSelectAll}>
+        {items.every((item) => item.checked)
+          ? 'Deselect all items'
+          : 'Select all items'}
       </button>
       <p className="text-right hidden lg:block">Price</p>
       <hr />
