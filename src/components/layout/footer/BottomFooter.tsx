38fc3ae22346825477a2ab3d73db6dca431a75dc
Shashanka Prajapati <shashankap95@gmail.com>
2021-07-01 20:36:21 +0545

    Fix: Open footer links in new tab

--- src/components/layout/footer/BottomFooter.tsx
+++ src/components/layout/footer/BottomFooter.tsx
@@ -5,12 +5,19 @@ const BottomFooter: React.FunctionComponent = () => {
     <section className="footer__bottom">
       <p className="footer__text">
         Amazon Clone created by{' '}
-        <a href="https://github.com/sp-95" className="footer__link">
+        <a
+          href="https://github.com/sp-95"
+          target="_blank"
+          rel="noreferrer"
+          className="footer__link"
+        >
           Shashanka Prajapati
         </a>{' '}
         with the help of{' '}
         <a
           href="https://www.youtube.com/user/ssangha32"
+          target="_blank"
+          rel="noreferrer"
           className="footer__link"
         >
           PAPA React
